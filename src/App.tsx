fdec3e59898412b0a552a146fd2ea3ce49508bbe
Akshat Mittal <akshatmittal2506@gmail.com>
2024-04-28 19:02:24 +0530

    feat: created library and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React from "react";
+import routes from "./routes";
 import "./styles/globals.scss";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 const App: React.FC = () => {
-	return <>React App</>;
+	return (
+		<>
+			<Routes>
+				{routes.map((route, index) => (
+					<Route
+						key={`route-${index}`}
+						path={route.path}
+						element={route.component}
+					/>
+				))}
+			</Routes>
+		</>
+	);
 };
 
 export default App;
