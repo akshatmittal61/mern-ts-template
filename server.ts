e55ece04cf3dc5398ee1b5ab444cf272e037db64
Akshat Mittal <akshatmittal2506@gmail.com>
2024-04-29 00:49:18 +0530

    feat: created config manager and boundary safety

--- server.ts
+++ server.ts
@@ -1,11 +1,15 @@
+import { PORT } from "./config";
 import express, { Request, Response } from "express";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/api/health", (_: Request, res: Response) => {
 	return res.status(200).json({ message: "API is healthy" });
 });
 
-app.listen(4000, () => {
-	console.info("Server is listening at http://localhost:4000");
+app.listen(PORT, () => {
+	console.info(`Server is listening at http://localhost:${PORT}`);
 });
