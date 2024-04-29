b036a7acebcf3df0859d51a25a703b471ba8a7e3
Akshat Mittal <akshatmittal2506@gmail.com>
2024-04-29 11:05:53 +0530

    feat: added middlewares

--- routes/blogs.route.ts
+++ routes/blogs.route.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import controllers from "../controllers";
+import middlewares from "../middlewares";
 
 const router = Router();
 
 router.route("/").get(controllers.blogs.getAllBlogs);
 router.route("/:id").get(controllers.blogs.getBlogById);
 
+router.use(middlewares.isLoggedIn);
 router.route("/").post(controllers.blogs.createBlog);
 router
 	.route("/:id")
